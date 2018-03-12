0bccbcfe75ca0ca3f5755bc416330887c4c7eecf
Daniele Conti <daniele.conti@novoda.com>
2018-03-12 20:05:29 +0000

    Use ObservableViewModel to cache pipelines

---
+++ app/src/main/java/com/example/florent/stateexperiment/ObservableViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.florent.stateexperiment
+
+import android.arch.lifecycle.ViewModel
+import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observables.ConnectableObservable
+import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.Subject
+
+abstract class ObservableViewModel<Input, Output> : ViewModel() {
+    private val input: Subject<Input> = PublishSubject.create()
+    private var output: ConnectableObservable<Output>? = null
+
+    private var disposable: Disposable? = null
+
+    fun caching(
+            source: Observable<Input>,
+            init: Observable<Input>.() -> Observable<Output>
+    ): Observable<Output> {
+        source.subscribe(input)
+
+        if (output == null) {
+            output = input.init().replay(1)
+        }
+
+        disposable = output!!.connect()
+        return output!!
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable?.dispose()
+    }
+}
